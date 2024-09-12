1c0d965fabede744b973033c313235680f07f8c5
Alecto <fomenkoandrey1978@gmail.com>
2024-09-12 21:14:20 +0300

    ex.5-7

--- src/helpers/fakeUsers.mjs
+++ src/helpers/fakeUsers.mjs
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { getRandomSkills } from './skillsData.mjs'
 
-export const users = Array.from({ length: 10000 }, () => {
+export const users = Array.from({ length: 100 }, () => {
   const shouldHaveOrders = Math.random() < 0.33
 
   const user = {
